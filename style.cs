f301c659486adb067a3279d891847872b9c0723d
iqra112 <162879444+iqra112@users.noreply.github.com>
2024-03-21 19:10:42 -0800

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,79 @@
+img{
+    height: 400px;
+    width: 400px;
+}
+*{
+    text-align: center;
+   
+
+}
+
+header{
+    background-color:  #D7D7D7;
+}
+.about_me{
+    margin-top: 100px;
+  margin-bottom: 100px;
+  background-color:  #FFEAEA;
+}
+Article{
+    margin-top: 100px;
+    margin-bottom: 100px;
+    background-color:  #EAF9FF;
+}
+p {
+    font-family: "Roboto", sans-serif;
+    font-weight: 300;
+    font-style: normal;
+    font-size: 16px;
+}
+h1{
+    font-family: "Roboto", sans-serif;
+    font-weight: 300;
+    font-style: normal;
+    font-size: 60px;
+}
+h2{
+    font-family: "Roboto", sans-serif;
+    font-weight: 300;
+    font-style: normal;
+    font-size: 42px;
+}
+h3{
+    font-family: "Roboto", sans-serif;
+    font-weight: 700;
+    font-style: normal;
+ font-size:36px;
+}
+body{
+    font-family: "Roboto", sans-serif;
+    font-weight: 300;
+    font-style: normal;
+    font-size: 21px;
+    margin: 0px;
+}
+.h1{
+    font-size: 80px;
+    /* color: #99D930; */
+    font-weight: bold    ;
+}
+.h2{
+    font-size: 60px;
+    /* color: #99D930; */
+    font-weight: bold;
+    
+}
+a{
+    
+    color: #99D930;
+}
+/* .roboto-light {
+    font-family: "Roboto", sans-serif;
+    font-weight: 300;
+  font-style: normal;
+  }
+  .roboto-black {
+    font-family: "Roboto", sans-serif;
+    font-weight: 900;
+    font-style: normal;
+  } */
